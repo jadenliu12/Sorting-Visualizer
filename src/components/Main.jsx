0895fdde2846007c768e63fa5e5562a9f1ff8eb8
Jaden <Jaden.1012@yahoo.com>
2021-07-20 23:58:37 +0800

    add nav buttons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
 import './Main.css';
 
@@ -9,9 +14,12 @@ export default class Main extends React.Component {
 
     render() {
         return (
-            <div className='main'>
-                <h1>Hello, World!</h1>
-            </div>
+            <Router>
+                <div className='main'>                    
+                    <div className='left'><button className='navBut'>+</button></div>
+                    <div className='right'><button className='navBut'>+</button></div>
+                </div>
+            </Router>
         )
     }
 }
