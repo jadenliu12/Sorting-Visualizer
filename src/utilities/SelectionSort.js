ff8d5953d4b34c57e1a5c554ecd93ca90e22d172
Jaden <Jaden.1012@yahoo.com>
2021-07-25 23:01:17 +0800

    add selection sort algo

---
+++ src/utilities/SelectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr, n) {        
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        for(let j = i+1; j < n; j++){
+            if(arr[j] < arr[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             // Swapping the elements
+             let tmp = arr[i]; 
+             arr[i] = arr[min];
+             arr[min] = tmp;      
+        }
+    }
+    return arr;
+}
